10095e639f16d2ebf2add85312919563351a6c8a
bleonardb3 <bleonardb3@gmail.com>
2019-05-15 22:59:49 -0400

    Add files via upload

--- FHT.py
+++ FHT.py
@@ -2,12 +2,12 @@ import urllib3, requests, json, os
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, RadioField, FloatField, IntegerField
+from wtforms import StringField, SubmitField, RadioField, FloatField, IntegerField,SelectField
 from wtforms.validators import Required, Length, NumberRange
 
-#url = 'https://ibm-watson-ml.mybluemix.net'
-#username = '1e31f23c-ad34-4927-8283-a55f66caec00'
-#password = 'ec54ced2-3909-4378-a42f-ee7e5081dd3d'
+#url = 'https://us-south.ml.cloud.ibm.com'
+#username = 'c49a6e9c-185e-446f-98df-9c4f9fb9aebd'
+#password = '766378de-306e-48eb-a191-d88b7115116a'
 
 if 'VCAP_SERVICES' in os.environ:
     vcap = json.loads(os.getenv('VCAP_SERVICES'))
@@ -17,15 +17,18 @@ if 'VCAP_SERVICES' in os.environ:
         username = creds['username']
         password = creds['password']
         url = creds['url']
-scoring_endpoint = 'https://us-south.ml.cloud.ibm.com/v3/wml_instances/02eafab5-7bd2-4ac4-abb1-334f0cc6232c/deployments/7536dc0f-1c2a-495c-9cb2-9ec299c24b0e/online'
+scoring_endpoint = 'https://us-south.ml.cloud.ibm.com/v3/wml_instances/02eafab5-7bd2-4ac4-abb1-334f0cc6232c/deployments/cbea645b-68ed-435b-b0e3-dc2f87df4eaf/online'
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretpassw0rd'
 bootstrap = Bootstrap(app)
 
 class FHTForm(FlaskForm):
-  categories = RadioField('Categories', coerce=int, choices=[('1','Sports/Travel'),('2','Engineering'),('3','Information Technology'),('4','Journalism'),('5','Government'),('6','Medical'), ('7','Science'),('8','Arts'),('9','Advertising'),('10','Legal'),('11','Construction'),('12','Retail'),('13','Education'),('14','Finance'),('15','Other')])
+  categories = SelectField('Categories', choices=[('Sports/Travel','Sports/Travel'),('Engineering','Engineering'),('Information Technology','Information Technology'),
+  ('Journalism','Journalism'),('Government','Government'),('Medical','Medical'), ('Science','Science'),('Arts','Arts'),('Advertising','Advertising'),('Legal','Legal'),
+  ('Construction','Construction'),('Retail','Retail'),('Education','Education'),('Finance','Finance'),('Other','Other')])
   age = IntegerField('Age:')
   countries_visited_count = IntegerField('Number of countries visited:')
+  passport_country = RadioField('Passport Country', choices=[('Ghana', 'Ghana'), ('Brazil', 'Brazil'), ('Pakistan', 'Pakistan'),('Bangladesh','Bangladesh'),('Haiti','Haiti'),('India','India')])
   submit = SubmitField('Submit')
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -39,32 +42,33 @@ def index():
     form.age.data = ''
     countries_visited_count = form.countries_visited_count.data
     form.countries_visited_count.data = ''
+    passport_country = form.passport_country.data
+    form.passport_country.data=''
         
     headers = urllib3.util.make_headers(basic_auth='{}:{}'.format(username, password))
     path = '{}/v3/identity/token'.format(url)
     response = requests.get(path, headers=headers)
     mltoken = json.loads(response.text).get('token')
     scoring_header = {'Content-Type': 'application/json', 'Authorization': 'Bearer' + mltoken}
-    payload_scoring = {"fields": ["COUNTRIES_VISITED_COUNT", "AGE", "Code"], "values": [[countries_visited_count,age,category]]}
+    payload = {"fields": ["COUNTRIES_VISITED_COUNT", "AGE","PASSPORT_COUNTRY", "Category"], "values": [[countries_visited_count,age,passport_country,category]]}
     scoring = requests.post(scoring_endpoint, json=payload, headers=scoring_header)
 
     scoringDICT = json.loads(scoring.text) 
     print ("scoringDICT: ",scoringDICT)
-   # scoringList = scoringDICT['values'].pop()[11:13]
-   # print (scoringList)
+    scoringList = scoringDICT['values'].pop()[6:9]
+    print (scoringList)
+    score  = int(scoringList[2])
+    probability = scoringList[0][int(scoringList[1])]
    # score = scoringList[1:].pop()
    # probability_died = scoringList[0:1].pop()[0:1].pop()
    # print (probability_died)
    # probability_survived = scoringList[0:1].pop()[1:].pop()
-   # if (score == 10.0) :
-   #   score_str = "high risk"
-   #   probability = probability_high_risk                                        
-   # else :
-   #   score_str = "not a high risk"
-   #   probability = probability_not_high_risk
-    score_str = "test"
-    probability = 1
-
+    if (score == 10) :
+      score_str = "high risk"
+    elif (score == 20) :
+      score_str = "medium risk"	   
+    else:
+      score_str = "low risk"
     return render_template('score.html', form=form, scoring=score_str,probability=probability)
   return render_template('index.html', form=form)
 port = os.getenv('PORT', '5000')
